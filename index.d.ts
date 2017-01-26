60b9c897ce3dcb3f5c40f8e68e0b7fe1cd3df4eb
Alex Kovalenko <koolse@outlook.com>
2017-01-26 13:55:12 +0200

    udpate typings

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,33 @@
 declare module 'rivets' {
+  // TODO: check if these are correct:
+  export interface Options {
+    // Attribute prefix in templates
+    prefix?: string;
+
+    //Preload templates with initial data on bind
+    preloadData?: boolean;
+
+    //Root sightglass interface for keypaths
+    rootInterface?: string;
+
+    // Template delimiters for text bindings
+    templateDelimiters?: Array<string>
+
+    // Augment the event handler of the on-* binder
+    handler?: Function;
+  }
+
   export interface Observer {
     unobserve: () => any
     value: () => any
   }
 
-  export interface View {
-    build(): void;
-    bind(): void;
-    unbind(): void;
+  export interface View extends Options {
+    models: Object
+    options: () => Options
+    build(): void
+    bind(): void
+    unbind(): void
     addBinding(node: HTMLElement, type: Binder<any> | string, declaration: string): Binding
   }
 
@@ -15,6 +35,9 @@ declare module 'rivets' {
     view: View
     unbind: () => any
     observe: (obj: Object, keypath: string, callback: (newValue) => void) => Observer
+    keypath: string
+    args: string[]
+    eventHandler: (handler: (event: Event) => any) => () => any
   }
 
   export interface FunctionalBinder<ValueType> {
@@ -22,18 +45,24 @@ declare module 'rivets' {
   }
 
   export interface Binder<ValueType> {
-    routine: FunctionalBinder<ValueType>
+    routine?: FunctionalBinder<ValueType>
     bind?: (this: Binding, element: HTMLElement) => void
     unbind?: (this: Binding, element: HTMLElement) => void
     update?: (this: Binding, model: ValueType) => void
     getValue?: (this: Binding, element: HTMLElement) => void
+    block?: boolean
+    function?: boolean
+  }
+
+  export interface Component {
+    template?: string
   }
 
   export interface Binders {
     string: Binder<any> | FunctionalBinder<any>
   }
 
-  export interface Rivets {
+  export interface Rivets extends Options{
     // Global binders.
     binders: Binders;
 
@@ -46,37 +75,9 @@ declare module 'rivets' {
     // Global sightglass adapters.
     adapters: Object;
 
-    // Default attribute prefix.
-    prefix: string;
-
-    // Default template delimiters.
-    templateDelimiters: Array<string>;
-
-    // Default sightglass root interface.
-    rootInterface: string;
-
-    // Preload data by default.
-    preloadData: boolean;
-
     handler(context: any, ev: Event, biding: any): void;
 
-    configure(options?: {
-
-      // Attribute prefix in templates
-      prefix?: string;
-
-      //Preload templates with initial data on bind
-      preloadData?: boolean;
-
-      //Root sightglass interface for keypaths
-      rootInterface?: string;
-
-      // Template delimiters for text bindings
-      templateDelimiters?: Array<string>
-
-      // Augment the event handler of the on-* binder
-      handler?: Function;
-    }): void;
+    configure(options?: Options): void;
 
     bind(element: HTMLElement, models: Object, options?: Object): View;
     // bind(element: JQuery, models: Object, options?: Object): View;
