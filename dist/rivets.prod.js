0a50597bcb6e73c5625e2339de57ea00a1428edb
Roman Nesteruk <r.nesteruk@viseven.com>
2017-10-03 15:55:07 +0300

    update(binders): fix rv-if observ

--- dist/rivets.prod.js
+++ dist/rivets.prod.js
@@ -1,5 +1,5 @@
 // Rivets.js
-// version: 1.0.2
+// version: 1.0.3
 // author: Michael Richards
 // license: MIT
 (function() {
@@ -1250,16 +1250,9 @@
       return (_ref1 = this.nested) != null ? _ref1.unbind() : void 0;
     },
     routine: function(el, value) {
-      var key, model, models, _ref1;
       if (!!value === !this.bound) {
         if (value) {
-          models = Object.create(this.view.models);
-          _ref1 = this.view.models;
-          for (key in _ref1) {
-            model = _ref1[key];
-            models[key] = model;
-          }
-          (this.nested || (this.nested = new Rivets.View(el, models, this.view.options()))).bind();
+          (this.nested || (this.nested = new Rivets.View(el, this.view.models, this.view.options()))).bind();
           this.marker.parentNode.insertBefore(el, this.marker.nextSibling);
           return this.bound = true;
         } else {
